d2160149950eeecac7de9096c97348182bb1fa2b
pneu <pneumaster@gmail.com>
2019-09-20 15:33:02 +0900

    backup
    提出の個数に制限があるみたいで、古いのが消えるようなので。

---
+++ abc003/a.hs
@@ -0,0 +1,69 @@
+--import Data.Ix
+import Control.Arrow
+import Control.Applicative
+import Control.Monad
+import Data.Maybe
+import Data.Word
+import Data.Bits
+import qualified Data.List as L
+import Data.Array.IO hiding (range)
+import Text.Printf
+import Foreign.Ptr
+import Foreign.Storable
+import Debug.Trace (trace)
+import Prelude hiding (cycle)
+ 
+debug x = trace (show x) x
+ 
+-- | Remove the stack size limit in the RTS (+RTS -K).
+unlimitStackSize :: IO ()
+unlimitStackSize = do
+  current <- peek maxStackSizePtr
+  when (current == 0x100000) $ -- default limit: 1M words
+    poke maxStackSizePtr (-1)
+ 
+maxStackSizePtr :: Ptr Word32
+maxStackSizePtr = plusPtr rtsFlagsPtr 12
+ 
+foreign import ccall "&RtsFlags" rtsFlagsPtr :: Ptr Word32
+ 
+readInt :: String -> Int
+readInt = read
+ 
+getInt :: IO Int
+getInt = getLine >>= return . readInt
+ 
+getInts :: IO [Int]
+getInts = getLine >>= return . map readInt . words
+ 
+tup :: [a] -> (a, a)
+tup (x:y:[]) = (x,y)
+ 
+uc :: (Integral a, Num b) => a -> b
+uc = fromIntegral
+ 
+toBits :: Integral a => Int -> [a]
+toBits = L.unfoldr (\b ->
+          if b == 0
+          then Nothing
+          else Just (fromIntegral $ b `mod` 2, b `div` 2))
+ 
+marray :: (Int, Int) -> Int -> IO (IOUArray Int Int)
+marray (a,b) x = newListArray (a,b) (repeat x)
+-- dist <- newListArray ((1,1),(n,n)) (repeat inf) :: IO (IOUArray (Int, Int) Int)
+-- let getDist = readArray dist :: (Int, Int) -> IO Int
+-- putDist = writeArray dist :: (Int, Int) -> Int -> IO ()
+ 
+marray' :: Ix i => (i,i) -> Int -> IO (IOUArray i Int)
+marray' (a,b) x = newListArray (a,b) (repeat x)
+ 
+pair :: (a -> b, a -> c) -> a -> (b, c)
+pair (f,g) x = (f x, g x)
+ 
+cross :: (a -> b, c -> d) -> (a, c) -> (b, d)
+cross (f,g) = pair (f.fst, g.snd)
+ 
+main :: IO ()
+main = do
+  n <- getInt
+  print . (flip div 2) . (*10000) . (+1) $ n
